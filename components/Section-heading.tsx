165ac53b631617eebe560bbfdd5f377ee44924da
ahmed ewees <ahmedewaisvi34@gmail.com>
2023-10-21 22:18:46 +0300

    hello form ahmed

--- components/Section-heading.tsx
+++ components/Section-heading.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-
-export default function ({children}) {
+type SectionHeadingProps={
+  children: React.ReactNode;
+}
+export default function ({children}:SectionHeadingProps ) {
   return <h1 className="capitalize mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl">
     {children}
   </h1>
